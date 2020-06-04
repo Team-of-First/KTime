be540d7844f80eaaea34133ed92a58658a7b7dce
milkid1412 <61926067+milkid1412@users.noreply.github.com>
2020-06-04 14:01:07 +0000

    시계로 땅을 터치시 시간이 뜹니다! 예) 11시 1분 17초 (PM) || It sows you time! (srry about no explaining XD)
    화이팅

---
+++ src/Kkevin14/KTime.php
@@ -0,0 +1,51 @@
+<?php
+namespace Kkevin14;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+
+use pocketmine\network\mcpe\protocol\ServerSettingsRequestPacket;
+use pocketmine\network\mcpe\protocol\ModalFormRequestPacket;
+use pocketmine\network\mcpe\protocol\ServerSettingsResponsePacket;
+use pocketmine\network\mcpe\protocol\ModalFormResponsePacket;
+
+use pocketmine\Player;
+
+class KTime extends PluginBase implements Listener{
+
+  public $id = [
+    11535263
+  ];
+
+  public $title = '§l§eSYSTEM §f|| §e현재시간';
+
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+
+  public function sendUI (Player $player, $code, $data) {
+    $packet = new ModalFormRequestPacket();
+    $packet->formId = $code;
+    $packet->formData = json_encode ($data);
+    $player->dataPacket ($packet);
+  }
+
+  public function onTouch(PlayerInteractEvent $event){
+    $player = $event->getPlayer();
+    $item = $event->getItem();
+    $date = date("h시 i분 s초 (A)");
+    if($item->getId() != 347) return;
+    $this->sendUI($player, $this->id[0], [
+      'type' => 'form',
+      'title' => $this->title,
+      'content' => "\n" . '§l§f현재시간 : ' . $date . "\n\n",
+      'buttons' => [
+        [
+          'text' => '§l§f[ §e시계 닫기 §f]'
+        ]
+      ]
+    ]);
+  }
+}
